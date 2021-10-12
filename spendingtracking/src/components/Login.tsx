af6169f03915f2ba6b51f2434fc10ade327027f0
Oğuz Önal <oguz.onal@biscozum.com>
2021-10-12 09:38:01 +0300

    Redux connection with Login Page

--- spendingtracking/src/components/Login.tsx
+++ spendingtracking/src/components/Login.tsx
@@ -3,25 +3,35 @@ import { useHistory, useLocation } from "react-router";
 import api from "../utils/api";
 import showError from "../utils/showError";
 import { Result } from "antd";
+import { LoginForm } from "../types/user";
+import { useDispatch, useSelector } from "react-redux";
+import { login } from "../store/actions/userActions";
+import { AppState } from "../store";
+import { useEffect } from "react";
+import showSuccess from "../utils/showSuccess";
 
 function Login() {
   const history = useHistory();
   const location = useLocation<{ newSignUp?: boolean }>();
+  const dispatch = useDispatch();
+  const { data, loading, error } = useSelector((state: AppState) => state.user);
 
-  const onFinish = async (values: any) => {
-    try {
-      await api.post("/users/login", values);
-      console.log("Success:", values);
-      history.push("/");
-    } catch (error: any) {
-      showError(error.response.data.errorMessage);
-    }
+  const onFinish = (values: LoginForm) => {
+    dispatch(login(values));
   };
 
-  const onFinishFailed = (errorInfo: any) => {
-    console.log("Failed:", errorInfo);
-    showError(errorInfo);
-  };
+  useEffect(() => {
+    error && showError(error);
+  }, [error]);
+
+  useEffect(() => {
+    data.username && showSuccess("You have successfully logged in!");
+  }, [data.username]);
+
+  useEffect(() => {
+    const token = localStorage.getItem("token");
+    token && history.push("/");
+  }, [data]);
 
   return (
     <Form
@@ -30,16 +40,11 @@ function Login() {
       wrapperCol={{ span: 16 }}
       initialValues={{ remember: true }}
       onFinish={onFinish}
-      onFinishFailed={onFinishFailed}
       autoComplete="off"
     >
       <h2 style={{ textAlign: "center", marginBottom: 40 }}>Please Login</h2>
       {location.state?.newSignUp && (
-        <Result
-          status="success"
-          title="You successfully signed up!"
-          subTitle="Please login using your credentials."
-        />
+        <Result status="success" title="You successfully signed up!" subTitle="Please login using your credentials." />
       )}
       <Form.Item label="Username" name="username" rules={[{ required: true, message: "Please input your username!" }]}>
         <Input />
